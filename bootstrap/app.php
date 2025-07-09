78b75c159fad89044681b2c5f5325964a01c9597
Yuki Higa <info@mar-cielo.com>
2025-07-10 00:59:48 +0900

    CSRFのエラーが発生した場合にログイン画面に遷移させる

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,10 @@ declare(strict_types=1);
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -21,5 +25,25 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->appendToGroup('api', \App\Http\Middleware\IpRestriction::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->respond(function (Response $response, Throwable $throwable, Request $request) {
+            if ($response->getStatusCode() === 419) {
+                // ログイン画面を表示させるためにログアウトを行う
+                Auth::logout();
+
+                // セッションの破棄
+                if ($request->hasSession()) {
+                    $request->session()->invalidate();
+                    $request->session()->regenerateToken();
+                }
+
+                // レスポンスのクラスの種類によって処理を切り分け
+                return $response instanceof JsonResponse
+                    // JsonResponse（application/json）の場合はログインに遷移させるためステータスコードを変更する
+                    ? $response->setStatusCode(401)
+                    // JsonResponseで無い場合はトップ画面にリダイレクトさせることでログイン画面を表示
+                    : redirect('/');
+            }
+
+            return $response;
+        });
     })->create();
